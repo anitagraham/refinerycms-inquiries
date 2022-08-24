4ad26ec330c9273b608aeba3690c0277038fe127
Anita <anita@joli.com.au>
2022-08-24 10:14:42 +0800

    Remove no-longer-required params from call to send_notification_email! Remove redundant params from call to send_confirmation_email!

--- lib/refinery/inquiries/spam_filter.rb
+++ lib/refinery/inquiries/spam_filter.rb
@@ -28,8 +28,8 @@ module Refinery
         end
 
         if notify?
-          send_notification_email!(@inquiry, @request)
-          send_confirmation_email!(@inquiry, @request)
+          send_notification_email!
+          send_confirmation_email!
         end
       end
 
@@ -89,7 +89,7 @@ module Refinery
         end
       end
 
-      def send_confirmation_email!(inquiry, request)
+      def send_confirmation_email!
         if Refinery::Inquiries::Setting.send_confirmation?
           begin
             InquiryMailer.confirmation(@inquiry, @request).deliver_now
